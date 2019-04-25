4fe77a5a54b6dc046642bc6f0c0161645cddd26f
Erika Davis <davis2erika@gmail.com>
2019-04-25 22:44:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,3 +11,8 @@ def first_element(array)
   my_first_element = ["Welcome to New York", "element 2"]
   return my_first_element.first
 end
+
+def third_element(array)
+  my_third_element = ["element 1", "element 2", "element 3", "style", "element 4"]
+  return my_third_element[3]
+end
